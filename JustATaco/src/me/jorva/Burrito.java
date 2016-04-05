4721f0c0b606f5b8eb6b977796aaf903b7a87e76
jorvacoding <jorvacoding@outlook.com>
2016-04-05 15:13:02 -0400

    The first commit of the battle between tacos and burritos

---
+++ JustATaco/src/me/jorva/Burrito.java
@@ -0,0 +1,10 @@
+package me.jorva;
+
+/**
+ * Created by jorva on 4/5/16.
+ */
+public class Burrito {
+    public static void main(String[] args){
+        System.out.println("Hello, I'm a burrito.");
+    }
+}
